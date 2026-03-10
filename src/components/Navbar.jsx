9d62a5521eaaa9a3082322a4a2ce7e19784aef74
Chirayu Mishra <chirayumishra24@gmail.com>
2026-03-10 13:51:25 +0530

    zoho link changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -159,7 +159,7 @@ export default function Navbar() {
                                 </div>
 
                                 <motion.a
-                                    href="https://forms.zohopublic.in/"
+                                    href="https://forms.zohopublic.in/skillizeecambridgecourtgroup1/form/PLAYBOXPRESCHOOL/formperma/wFHW4FGSaBDZ7zJftITO1SELaO1h6OtlQJ960NlCIIA"
                                     target="_blank"
                                     rel="noopener noreferrer"
                                     className="clay-btn clay-btn-primary mobile-enroll-btn"
